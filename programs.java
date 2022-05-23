5b5a858793b0a66cf7e91333761e2b165b0bd92f
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-05-23 22:58:34 +0530

    cartesianuc2

--- programs.java
+++ programs.java
@@ -1,15 +1,19 @@
 import java.util.*;
-public class cartesianuc2 {
+public class cartesianuc3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter length of first line");
-        int a1 = sc.nextInt();
-        System.out.print("Enter length of  second line");
-        int b1 = sc.nextInt();
-        if (a1 == b1)
-            System.out.println("both lines are equal");
+        System.out.print("Enter length of first l1");
+        int l1 = sc.nextInt();
+        System.out.print("Enter length of second l2");
+        int l2 = sc.nextInt();
+
+        if (l1 > l2) {
+            System.out.print( " l1 is greater then l2");
+        }
+        else if (l1 < l2){
+            System.out.print( " l2 is lesser then l2");
+        }
         else
-            System.out.println("both lines are not equal");
+            System.out.print( "both lines are equal");
     }
 }
-
