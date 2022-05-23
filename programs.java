f6b98e9fafe06acb1539ca0dcee735853bd8530d
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-05-23 22:48:20 +0530

    empwageuc3

--- programs.java
+++ programs.java
@@ -1,12 +1,16 @@
-public class empwageuc2{
-    public static void main(String[] args) {
-       int  ispresent=1;
-       int rateperhour=20;
-       int mysalary=0;
-       int hours=8;
+public class empwageuc3{
+ public static void main(String[] args) {
+        int fulltime=1;
+        int parttime=0;
+        int rateperhour=20;
+        int emphrs=0;
+        int mysalary=0;
         double empcheck = Math.floor(Math.random() * 10) % 2;
-       if (ispresent == empcheck)
-     mysalary=(rateperhour * hours);
+       if (fulltime == empcheck)
+            emphrs=8;
+       else if ( parttime == empcheck)
+             emphrs=4;
+	mysalary=(rateperhour * emphrs);
        System.out.println(mysalary);
     }
 }
