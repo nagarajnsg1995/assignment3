36bad86d88766c5c3aa5d161938e31dd90939518
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-05-24 21:28:18 +0530

    emp20days

--- programs.java
+++ programs.java
@@ -1,28 +1,46 @@
-public class empwageuc4 {
 
 
+
+public class empwageuc4
+{
     public static void main(String[] args) {
-        int rateperhrs= 20;
-        int fulltime= 1;
-        int parttime= 2;
-        int empwage= 0;
+        int Rateperhrs = 20;
+        int Fulltime = 1;
+        int Parttime = 2;
+        int empwage = 0;
         int emphr = 0;
-        int empcheck = (int)Math.floor(Math.random()*10)%3;
-        switch (empcheck)
-        {
-            case 1:
-                emphr = 4;
-                break;
+        int workdays = 20;
+        int totalWage = 0;
 
-            case 2:
-                emphr = 8;
-                break;
+        for ( int days = 0; days<=workdays; days++ ) {
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            default:
-                emphr =0;
-                break;
+            switch (empcheck) {
+                case 1:
+                    emphr = 4;
+                    break;
+
+                case 2:
+                    emphr = 8;
+                    break;
+
+                default:
+                    emphr = 0;
+                    break;
+            }
+            empwage = (emphr * Rateperhrs);
+            totalWage = empwage;
+            System.out.println(empwage);
         }
-        empwage =  ( emphr * rateperhrs );
-        System.out.println ( empwage );
+            System.out.println(totalWage);
+
+
     }
 }
+
+
+
+
+
+
+
