a84d8da3c5d67a341d00e1f60a1313950e2b1283
nagarajnsg1995 <104911894+nagarajnsg1995@users.noreply.github.com>
2022-06-02 22:00:44 +0530

    Update programs.java

--- programs.java
+++ programs.java
@@ -1,15 +1,25 @@
 import java.util.*;
-public class cartesianuc2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter length of first line");
-        int a1 = sc.nextInt();
-        System.out.print("Enter length of  second line");
-        int b1 = sc.nextInt();
-        if (a1 == b1)
-            System.out.println("both lines are equal");
-        else
-            System.out.println("both lines are not equal");
-    }
-}
 
+public class CompareLine {
+        public static final int x1=6,x2=1,y1=5,y2=1;
+        public static final int a1=6,a2=1,b1=5,b2=1;
+
+        public static void main(String[] args)
+        {
+
+            double line1=Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)*1);
+            double line2=Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2)*1);
+
+            System.out.println("length of line1 is"+line1);
+            System.out.println("length of line2 is"+line2);
+
+           if (line1 == line2){
+               System.out.println("lines are equals");
+              
+           }
+            else
+                System.out.println("lines are not equals")
+
+    }
+
+}
