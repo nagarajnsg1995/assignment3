fd9e0efa9236ff72dd62247fdc3dc594aefab8b7
nagarajnsg1995 <104911894+nagarajnsg1995@users.noreply.github.com>
2022-06-02 21:57:38 +0530

    Update programs.java

--- programs.java
+++ programs.java
@@ -1,19 +1,24 @@
 import java.util.*;
-public class cartesianuc3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter length of first l1");
-        int l1 = sc.nextInt();
-        System.out.print("Enter length of second l2");
-        int l2 = sc.nextInt();
+public class CompareLine {
+    public static final int x1=1,x2=1,y1=1,y2=1;
+    public static final int a1=2,a2=1,b1=8,b2=1;
 
-        if (l1 > l2) {
-            System.out.print( " l1 is greater then l2");
+    public static void main(String[] args) {
+
+        double line1=Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)*1);
+        double line2=Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2)*1);
+
+        System.out.println("length of line1 is"+line1);
+        System.out.println("length of line2 is"+line2);
+
+        if (line1>line2){
+            System.out.println("line1 is greater then line2");
         }
-        else if (l1 < l2){
-            System.out.print( " l2 is lesser then l2");
+       else if (line2>line1){
+            System.out.println("line2 is greater then line1");
         }
-        else
-            System.out.print( "both lines are equal");
+       else
+            System.out.println("both lines are equal");
+
     }
 }
